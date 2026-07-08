fe7e993fc7106daa596ccfe42fa507faf2ea8eac
starch <starchpersonal@gmail.com>
2026-07-08 16:40:39 -0400

    these lines are too fucking long

--- Content.Server/_Mono/Radar/RadarBlipSystem.cs
+++ Content.Server/_Mono/Radar/RadarBlipSystem.cs
@@ -137,7 +137,7 @@ public sealed partial class RadarBlipSystem : EntitySystem
                 var missileArcOffset = MathHelper.DegreesToRadians(seeker.ScanArc / 2);
                     _tempMissileCache.Add(new(netBlipUid,
                         GetNetCoordinates(coord),
-                        seeker.MaxSpeed * 0.4,
+                        seeker.MaxSpeed * 0.2,
                         missileRotation,
                         blipVelocity,
                         seeker.IdleColor));
@@ -146,14 +146,14 @@ public sealed partial class RadarBlipSystem : EntitySystem
                     {
                         _tempMissileCache.Add(new(netBlipUid,
                             GetNetCoordinates(coord),
-                            seeker.MaxSpeed * 0.9,
+                            seeker.MaxSpeed * 0.3,
                             missileRotation - missileArcOffset,
                             blipVelocity,
                             seeker.ArcLinesColor));
 
                         _tempMissileCache.Add(new(netBlipUid,
                             GetNetCoordinates(coord),
-                            seeker.MaxSpeed * 0.9,
+                            seeker.MaxSpeed * 0.3,
                             missileRotation + missileArcOffset,
                             blipVelocity,
                             seeker.ArcLinesColor));
